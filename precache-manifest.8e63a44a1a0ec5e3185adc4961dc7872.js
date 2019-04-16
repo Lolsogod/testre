c572356d7ea0dceccb68bb395d412280b2388e1a
Lolsogod <46161640+Lolsogod@users.noreply.github.com>
2019-04-16 22:00:03 +0400

    Add files via upload

---
+++ precache-manifest.8e63a44a1a0ec5e3185adc4961dc7872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71bbfcc52ee5396023b69c33a3aee97d",
+    "url": "/static/media/theme.71bbfcc5.mp3"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7bb0c33a37a5c5b98c92",
+    "url": "/static/js/main.6d38202d.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "7bb0c33a37a5c5b98c92",
+    "url": "/static/css/main.3c4bdd4c.chunk.css"
+  },
+  {
+    "revision": "a7c84998b416fb3d3ad210439fb00b5a",
+    "url": "/index.html"
+  }
+];
